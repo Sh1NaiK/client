157fe492c60773c66a54f885d6c16a07517b71ba
Sh1NaiK <115789053+Sh1NaiK@users.noreply.github.com>
2022-12-08 09:03:25 +0300

    transactions update

--- src/components/transactions.tsx
+++ src/components/transactions.tsx
@@ -1,4 +1,12 @@
-import { Box, Button } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  MenuItem,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import Navbar from "./navbar";
@@ -59,8 +67,8 @@ const Transactions = () => {
     })();
   });
 
-  const addTransactionHandler = async () => {
-    await fetch("/api/transactions/add", {
+  const addTransactionHandler = () => {
+    fetch("/api/transactions/add", {
       method: "POST",
       body: JSON.stringify({
         jwt: localStorage.getItem("auth-token"),
@@ -85,34 +93,72 @@ const Transactions = () => {
   const { Column } = Table;
 
   interface DataType {
-    id: React.Key;
+    id: string;
     iso_want: string;
     iso_sell: string;
     rate: number;
     amount: number;
-    user_id: React.Key;
+    user_id: string;
   }
 
   const data: DataType[] = dbData;
 
-  const deleteTransactionHandler = async () => {
-    console.log("delete");
-    // const response = await fetch("/api/transactions/delete", {
-    //   method: "DELETE",
-    //   body: JSON.stringify({
-    //     jwt: localStorage.getItem("auth-token"),
-    //   }),
-    //   headers: headers,
-    // }).then((response) => {
-    //   if (response.ok) {
-    //     return response.json();
-    //   }
-    //   return response.text().then((error) => {
-    //     const e = new Error("Something wrong...");
-    //     e.message = error;
-    //     throw e;
-    //   });
-    // });
+  const deleteTransactionHandler = async (id: string) => {
+    await fetch("/api/transactions/delete", {
+      method: "DELETE",
+      body: JSON.stringify({
+        jwt: localStorage.getItem("auth-token"),
+        id: id,
+      }),
+      headers: headers,
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response.text().then((error) => {
+        const e = new Error("Something wrong...");
+        e.message = error;
+        throw e;
+      });
+    });
+  };
+
+  const currencies = [
+    {
+      value: "USD",
+      label: "USD",
+    },
+    {
+      value: "EUR",
+      label: "EUR",
+    },
+    {
+      value: "RUB",
+      label: "RUB",
+    },
+    {
+      value: "BYN",
+      label: "BYN",
+    },
+  ];
+
+  const [isoWant, setIsoWant] = useState("");
+  const [isoSell, setIsoSell] = useState("");
+  const [rate, setRate] = useState("");
+  const [amount, setAmount] = useState("");
+
+  // const regex = /^\D*\.?\D*$/;
+  // const regex = /((\d+)((\.\d{1,2}?))$/;
+
+  const handleChangeRate = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    // setRate(value.replace(/[^\d.]/g, ""));
+    setRate(value.replace(regex, ""));
+  };
+
+  const handleChangeAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setAmount(value.replace(/\D/g, ""));
   };
 
   return (
@@ -137,29 +183,126 @@ const Transactions = () => {
         <Box
           className="formContainer"
           sx={{
+            display: "flex",
             backgroundColor: "whitesmoke",
-            height: "70%",
+            height: "84%",
             width: "60%",
             margin: "0 auto",
             padding: "0.5rem 1rem 0 1rem",
           }}
         >
-          <Table dataSource={data}>
-            <Column title="ISO want" dataIndex="iso_want" key="iso_want" />
-            <Column title="ISO sell" dataIndex="iso_sell" key="iso_sell" />
-            <Column title="Rate" dataIndex="rate" key="rate" />
-            <Column title="Amount" dataIndex="amount" key="amount" />
-            <Column
-              title="Action"
-              key="action"
-              render={(_: any, record: DataType) => (
-                <Space size="middle">
-                  <Button onClick={deleteTransactionHandler}>Delete</Button>
-                </Space>
-              )}
-            />
-          </Table>
-          <Button onClick={addTransactionHandler}>Add</Button>
+          <Box
+            className="tableContainer"
+            sx={{
+              width: "70%",
+            }}
+          >
+            <Table dataSource={data} rowKey="id">
+              <Column title="ISO want" dataIndex="iso_want" key="iso_want" />
+              <Column title="ISO sell" dataIndex="iso_sell" key="iso_sell" />
+              <Column title="Rate" dataIndex="rate" key="rate" />
+              <Column title="Amount" dataIndex="amount" key="amount" />
+              <Column
+                title="Action"
+                key="action"
+                render={(_: any, record: DataType) => (
+                  <Space size="middle">
+                    <Button onClick={() => deleteTransactionHandler(record.id)}>
+                      Delete
+                    </Button>
+                  </Space>
+                )}
+              />
+            </Table>
+          </Box>
+          <Box
+            className="addContainer"
+            sx={{
+              textAlign: "center",
+              margin: "auto 0 auto 2rem",
+              padding: "1rem 0 1rem 0",
+              width: "26%",
+              borderRadius: "1rem",
+              backgroundColor: "white",
+            }}
+          >
+            <FormControl fullWidth>
+              <Typography
+                variant="h5"
+                sx={{
+                  margin: "0 auto 1rem auto",
+                }}
+              >
+                Add new transaction
+              </Typography>
+              <TextField
+                id="outlined-select-currency"
+                select
+                label="Wanted ISO"
+                value={isoWant}
+                helperText="Select wanted currency"
+                sx={{
+                  margin: "0 auto 0.5rem auto",
+                  width: "10.1rem",
+                }}
+              >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                id="outlined-select-currency"
+                select
+                label="Sell ISO"
+                value={isoSell}
+                helperText="Select sell currency"
+                sx={{
+                  margin: "0 auto 0.5rem auto",
+                  width: "10.1rem",
+                }}
+              >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                id="rate"
+                label="Rate"
+                value={rate}
+                onChange={handleChangeRate}
+                variant="outlined"
+                sx={{
+                  margin: "0 auto 0.5rem auto",
+                  width: "10.1rem",
+                }}
+              />
+              <TextField
+                id="amount"
+                label="Amount"
+                value={amount}
+                onChange={handleChangeAmount}
+                variant="outlined"
+                sx={{
+                  margin: "0 auto 0.5rem auto",
+                  width: "10.1rem",
+                }}
+              />
+            </FormControl>
+            <Button
+              onClick={addTransactionHandler}
+              sx={{
+                borderRadius: "1rem",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+            >
+              Add Transaction
+            </Button>
+          </Box>
         </Box>
       </Box>
     </>
